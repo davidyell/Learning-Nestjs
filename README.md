a3ec3823ec3b85d8496d3954b756c1519da9b152
David Yell <david.yell@mdgroup.com>
2022-01-25 15:41:43 +0000

    Completed the tutorial video

---
+++ README.md
@@ -0,0 +1,5 @@
+# Learning Nest.js
+Code from learning basic Nest.js, following the Academind tutorial 
+
+https://www.youtube.com/watch?v=F_oOtaxb0L8
+
---
+++ src/products/products.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Post, Get, Patch, Delete, Body, Param } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+    
+    constructor(private readonly productsService: ProductsService) {}
+
+    @Post()
+    addProduct(
+        @Body('title') prodTitle: string, 
+        @Body('description') prodDesc: string, 
+        @Body('price') prodPrice: number
+    ): any {
+        const newProductId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
+
+        return { id: newProductId };
+    }
+
+    @Get()
+    getAllProducts() {
+        return this.productsService.getProducts();
+    }
+
+    @Get(':id')
+    getProduct(@Param('id') id: string) {
+        return this.productsService.getSingleProduct(id);
+    }
+
+    @Patch(':id')
+    updateProduct(
+        @Param('id') prodId: string,
+        @Body('title') prodTitle: string, 
+        @Body('description') prodDesc: string, 
+        @Body('price') prodPrice: number
+    ) {
+        this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+        return null;
+    }
+
+    @Delete(':id')
+    removeProduct(@Param('id') prodId: string) {
+        this.productsService.removeProduct(prodId);
+        return null;
+    }
+}
