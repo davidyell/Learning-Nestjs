14bb3fdd83a968d92d1f6cb70364dd8978ba94ba
David Yell <david.yell@mdgroup.com>
2022-01-26 10:07:03 +0000

    Added controller methods for users module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { UserCreateDto } from './dtos/user.create.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -8,5 +16,20 @@ export class UsersController {
   @Get()
   list() {
     return this.usersService.list();
+  }
+
+  @Get(':id')
+  async view(@Param('id') id: number) {
+    const user = await this.usersService.findById(id);
+    if (user) {
+      return user;
+    }
+
+    throw new NotFoundException('User not found');
+  }
+
+  @Post()
+  create(@Body() createUserDto: UserCreateDto) {
+    return this.usersService.create(createUserDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { UserCreateDto } from './dtos/user.create.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -13,7 +14,12 @@ export class UsersService {
     return this.usersRepository.find();
   }
 
-  findById(id: number): Promise<User> {
-    return this.usersRepository.findOneOrFail(id);
+  async findById(id: number): Promise<User> {
+    return await this.usersRepository.findOne(id);
+  }
+
+  create(createUserDto: UserCreateDto): Promise<User> {
+    const user = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(user);
   }
 }
