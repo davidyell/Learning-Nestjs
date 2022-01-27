8a0241423084268daaaba93855e984a492eb6c0f
David Yell <neon1024@gmail.com>
2022-01-27 12:33:00 +0000

    Add the snake_case naming strategy
    This avoids needing to have capsCase foreign keys in the database, which I don't like personally

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,14 @@
+const { SnakeNamingStrategy } = require('typeorm-naming-strategies');
+
 module.exports = {
-    name: 'default',
-    type: 'better-sqlite3',
-    fileMustExist: true,
-    database: 'database.sqlite3',
-    entities: ['dist/**/entities/*.entity.js'],
-    migrations: ['dist/db/migrations/*.js'],
-    cli: {
-      migrationsDir: 'src/db/migrations',
-    },
-}
+  name: 'default',
+  type: 'better-sqlite3',
+  fileMustExist: true,
+  database: 'database.sqlite3',
+  entities: ['dist/**/entities/*.entity.js'],
+  migrations: ['dist/db/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/db/migrations',
+  },
+  namingStrategy: new SnakeNamingStrategy(),
+};
